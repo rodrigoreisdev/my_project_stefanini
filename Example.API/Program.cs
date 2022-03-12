7920c4c8dc3525a69aa207850a409deaf73c8625
Rodrigo Antonio dos Reis <67196386+rodrigoreisdev@users.noreply.github.com>
2022-03-12 19:44:08 -0300

    Comit III

--- Example.API/Program.cs
+++ Example.API/Program.cs
@@ -10,7 +10,15 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+            {
+                options.CustomSchemaIds(x => x.FullName);
+
+                options.CustomOperationIds(e =>
+                {
+                    return $"{e.ActionDescriptor.RouteValues["controller"]}_{e.HttpMethod}{e.ActionDescriptor.Parameters?.Select(a => a.Name).ToString()}";
+                });
+            });
 
 builder.Services.AddScoped<IPersonService, PersonService>();
 builder.Services.AddDbContext<PersonContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -23,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 using (var scope = app.Services.CreateScope())
 {
